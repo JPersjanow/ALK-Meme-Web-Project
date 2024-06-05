18cdb6da3038ae54b3b9ebaed79c422fd0a2383c
„Kostek095” <michal.k095@gmail.com>
2024-06-05 15:14:35 +0200

    clearout code

--- src/pages/AddMemePage.jsx
+++ src/pages/AddMemePage.jsx
@@ -3,7 +3,7 @@ import MemeFormComponent from "../components/MemeFormComponent";
 export const AddMemePage = () => {
   return (
     <div className="">
-      <h2 className="titlepage">Add your own Meme</h2>
+      <h2 className="">Add your own Meme</h2>
       <MemeFormComponent></MemeFormComponent>
     </div>
   );
