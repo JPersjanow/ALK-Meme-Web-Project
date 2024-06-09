334937c23177af1cd4dc7369d7a0e7679d16a84e
Kostek095 <126886503+Kostek095@users.noreply.github.com>
2024-06-09 19:45:05 +0200

    Update RegularPage.jsx

--- src/pages/RegularPage.jsx
+++ src/pages/RegularPage.jsx
@@ -5,12 +5,10 @@ export const RegularPage = () => {
   return (
     <div className="bg-[url('https://merkuriusz.pl/wp-content/uploads/2024/06/membgc.png')] bg-repeat" >
       <div className="titlepage">
-
       <h2>Regular Page</h2>
       <div className="title-page-icon text-green-500">
           <BiSolidHome></BiSolidHome>
         </div>
-
       </div>
       <MemeListComponent></MemeListComponent>
     </div>
