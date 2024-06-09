5acabba3654d0db75317d28ff1a26c61fb49a896
Kostek095 <126886503+Kostek095@users.noreply.github.com>
2024-06-09 19:42:15 +0200

    Update ContactComponent.jsx
    usunięcie zbędnych wierszy

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -2,7 +2,6 @@ import { IoLogoLinkedin } from "react-icons/io5";
 
 export const ContactComponent = ({ namePerson, urlPerson, urlPersonImg }) => {
   return (
-    
       <div className="person-module">
         <img className="img-person-module" src={urlPersonImg} alt="" />
         <h2>{namePerson}</h2>
@@ -10,6 +9,5 @@ export const ContactComponent = ({ namePerson, urlPerson, urlPersonImg }) => {
           <IoLogoLinkedin></IoLogoLinkedin>
         </a>
       </div>
-    
   );
 };
