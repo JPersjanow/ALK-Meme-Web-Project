5634484039400ee1fba3eba08ce90176b1e60db4
Kuba Persjanow <jakubpersjanow@gmail.com>
2024-06-11 01:34:03 +0200

    Updated error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,20 @@
+import { MdError } from "react-icons/md";
+import MemeBackground from "../assets/meme-background.png";
+
+
 export const ErrorPage = () => {
     return (
         <div>
-          <div className="titlepage">
-          <h2>Oooops! Error</h2>
+          <div
+            className="background"
+            style={{ backgroundImage: `url(${MemeBackground})` }}
+          >
+            <div className="title-header--red-shadow">
+              <h2>Ooops! I think we got an error</h2>
+              <div className="title-header-icon text-red-500">
+                <MdError></MdError>
+              </div>
+            </div>
           </div>
         </div>
       );
