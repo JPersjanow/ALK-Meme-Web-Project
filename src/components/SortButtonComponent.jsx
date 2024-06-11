225c852519393b17aca438c01769ea8dc6231455
Kuba Persjanow <jakubpersjanow@gmail.com>
2024-06-11 12:35:32 +0200

    further styling fixes

--- src/components/SortButtonComponent.jsx
+++ src/components/SortButtonComponent.jsx
@@ -2,13 +2,13 @@ import { FaArrowAltCircleDown, FaArrowAltCircleUp } from "react-icons/fa";
 
 export const SortButtonComponent = ({ sortState, handleFunction }) => {
   return (
-    <div onClick={handleFunction} className="button-sort group">
+    <button className="button-sort group" onClick={handleFunction}>
       {sortState ? (
         <FaArrowAltCircleDown className="button-sort-icon group-hover:rotate-180" />
       ) : (
         <FaArrowAltCircleUp className="button-sort-icon group-hover:rotate-180" />
       )}
-      <button>{sortState ? "Show newest" : "Show oldest"}</button>
-    </div>
+      {sortState ? "Show newest" : "Show oldest"}
+    </button>
   );
 };
