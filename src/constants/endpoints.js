7f8a6a9951dc6906fa024936c6c0cb914f9eeabe
Kuba Persjanow <jakubpersjanow@gmail.com>
2024-06-13 03:29:57 +0200

    Deleted unused endpoints in endpoints.js

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -3,8 +3,5 @@ export const MEMES = `http://localhost:${port}/memes`;
 export const MEMES_BY_ADDED_BY = (username) =>
   `http://localhost:${port}/memes?added_by=${username}`;
 export const MEME = (id) => `http://localhost:${port}/memes/${id}`;
-export const USER = (username) =>
-  `http://localhost:${port}/users?username=${username}`;
-export const USER_BY_ID = (id) => `http://localhost:${port}/users?id=${id}`;
 export const LOGIN = (username, password) =>
   `http://localhost:${port}/users?username=${username}&password=${password}`;
