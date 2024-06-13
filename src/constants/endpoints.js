93268d34d48785415a1343164dde308848392925
Kuba Persjanow <jakubpersjanow@gmail.com>
2024-06-13 10:21:26 +0200

    fixed bug with incorrect login notifications and bug with displaying user memes

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -3,5 +3,5 @@ export const MEMES = `http://localhost:${port}/memes`;
 export const MEMES_BY_ADDED_BY = (username) =>
   `http://localhost:${port}/memes?added_by=${username}`;
 export const MEME = (id) => `http://localhost:${port}/memes/${id}`;
-export const LOGIN = (username, password) =>
-  `http://localhost:${port}/users?username=${username}&password=${password}`;
+export const USER = (username) =>
+  `http://localhost:${port}/users?username=${username}`;
