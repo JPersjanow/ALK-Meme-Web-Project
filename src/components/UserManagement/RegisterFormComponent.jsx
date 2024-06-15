6ea55773366eae2715c1e0daa45b6898ce468527
Kuba Persjanow <jakubpersjanow@gmail.com>
2024-06-15 12:26:03 +0200

    Cleaned up RegisterFormComponent.jsx

--- src/components/UserManagement/RegisterFormComponent.jsx
+++ src/components/UserManagement/RegisterFormComponent.jsx
@@ -1,10 +1,10 @@
-import { IoLogIn } from "react-icons/io5";
 import * as constants from "../../constants/index.js";
 import axios from "axios";
 import { notifyError, notifySuccess } from "../Notifications/ToastNotification";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import { FaUserPlus } from "react-icons/fa6";
 
 export const RegisterFormComponent = () => {
   const [usernameInputStyleModifier, setUsernameInputStyleModifier] =
@@ -18,6 +18,42 @@ export const RegisterFormComponent = () => {
   const [cookies, setCookies] = useCookies();
   const navigate = useNavigate();
 
+  const returnUserInputsStatus = (userInputs) => {
+    let userInputStatus = true;
+    if (userInputs.passwordValue.length < 8) {
+      notifyError("Password must be longer than 8 characters");
+      setPasswordInputStyleModifier("--error");
+      userInputStatus = false;
+    }
+    if (userInputs.usernameValue.length === 0) {
+      notifyError("Username can't be empty");
+      setUsernameInputStyleModifier("--error");
+      userInputStatus = false;
+    }
+    if (userInputs.firstNameValue.length === 0) {
+      notifyError("First Name can't be empty");
+      setFirstNameInputStyleModifier("--error");
+      userInputStatus = false;
+    }
+    if (userInputs.lastNameValue.length === 0) {
+      setLastNameInputStyleModifier("--error");
+      notifyError("Last Name can't be empty");
+      userInputStatus = false;
+    }
+
+    return userInputStatus;
+  }
+
+  const registerUser = (usernameValue, lastNameValue) => {
+    setCookies(constants.cookies.COOKIE_USER_LOGGED, true);
+    setCookies(constants.cookies.COOKIE_USER_DATA, {
+      username: usernameValue,
+      lname: lastNameValue,
+    });
+    notifySuccess("Registered! Hello you!");
+    navigate(constants.routes.USERPAGE);
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -26,28 +62,14 @@ export const RegisterFormComponent = () => {
     const usernameValue = event.target[2].value;
     const passwordValue = event.target[3].value;
 
-    let inputErrors = false;
+    const inputsStatus = returnUserInputsStatus({
+      firstNameValue: firstNameValue,
+      lastNameValue: lastNameValue,
+      usernameValue: usernameValue,
+      passwordValue: passwordValue});
 
-    if (passwordValue.length < 8) {
-      notifyError("Password must be longer than 8 characters");
-      setPasswordInputStyleModifier("--error");
-
-      inputErrors = true;
-    }
-
-    if (firstNameValue.length === 0) {
-      notifyError("First Name can't be empty");
-      setFirstNameInputStyleModifier("--error");
-      inputErrors = true;
-    }
-    if (lastNameValue.length === 0) {
-      setLastNameInputStyleModifier("--error");
-      notifyError("Last Name can't be empty");
-      inputErrors = true;
-    }
-
-    if (inputErrors) {
-      return 0;
+    if (!inputsStatus) {
+      return 1;
     }
 
     axios
@@ -61,24 +83,24 @@ export const RegisterFormComponent = () => {
             lname: lastNameValue,
           };
 
-          return axios.post("http://localhost:3000/users", payload);
+          return axios.post(constants.endpoints.USERS, payload);
         } else {
-          notifyError("Username already taken");
-          setUsernameInputStyleModifier("--error");
+          throw new Error("Username already taken");
         }
       })
       .then((response) => {
-        notifySuccess("Registered");
-        setCookies(constants.cookies.COOKIE_USER_LOGGED, true);
-        setCookies(constants.cookies.COOKIE_USER_DATA, {
-          username: usernameValue,
-          lname: lastNameValue,
-        });
-        notifySuccess("Hello you! Nice logging");
-        navigate(constants.routes.USERPAGE);
+        registerUser(usernameValue, lastNameValue);
       })
       .catch((error) => {
-        console.log(error);
+        if(error.message === "Username already taken") {
+          notifyError(error.message);
+          setUsernameInputStyleModifier("--error");
+        } else {
+          notifyError("Error occurred while registering user");
+          navigate(constants.routes.ERRORROUTE, {
+            state: { error: error.message, errorCode: error.code },
+          });
+        }
       });
   };
 
@@ -119,7 +141,7 @@ export const RegisterFormComponent = () => {
           placeholder="Password"
         ></input>
         <button className="button-login group" type="submit">
-          <IoLogIn className="button-submit-icon group-hover:text-2xl"></IoLogIn>
+          <FaUserPlus className="button-submit-icon group-hover:text-2xl"></FaUserPlus>
           Register
         </button>
       </form>
